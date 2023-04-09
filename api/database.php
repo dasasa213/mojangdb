e1de83d33527042aa6d37fbf816e451ab64e6042
dasasa213 <130129294+dasasa213@users.noreply.github.com>
2023-04-09 22:05:49 +0900

    DB修正

--- api/database.php
+++ api/database.php
@@ -1,6 +1,6 @@
 <?php
-$host = 'localhost';
-$username = 'root';
-$password = 'OKB_dikc_0912';
-$dbname = 'mojang';
+$host = 'ec2-3-93-160-246.compute-1.amazonaws.com';
+$username = 'bclymgxatvkjbh';
+$password = 'f6cfd4ccae3f2c5143b42389a0a53dbdf2f44a4eb9b17463ceddb3a9c78282f6';
+$dbname = 'd49d9t9bi6jmun';
 ?>
