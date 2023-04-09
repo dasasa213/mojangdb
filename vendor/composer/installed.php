8101e0562cd4fb39e4761fef17f7c87436941679
dasasa213 <130129294+dasasa213@users.noreply.github.com>
2023-04-09 16:36:37 +0900

    again

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e3d3bdb0aebfa4b2966dd02d1ca366ddd49ebcb0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e3d3bdb0aebfa4b2966dd02d1ca366ddd49ebcb0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
